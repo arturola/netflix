ce3c1b784d16b7c12447b1a41087134a65b42d77
arturolatorre <arturola@ymail.com>
2020-11-10 14:07:07 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
+import Nav from "./Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Nav />
+      </div>
+
+      <div>
+        <Banner />
+      </div>
+
+      <div>
+        <Row
+          title="NETFLIX ORIGINALS"
+          fetchUrl={requests.fetchNetflixOriginals}
+          isLargeRow={true}
+        />
+        <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
